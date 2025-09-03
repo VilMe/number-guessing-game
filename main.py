a873d61f05ef8c1f00e052c08bad7fd74e7887b2
VilMe <vilma.medrano00@gmail.com>
2025-09-02 23:48:19 -0500

    add infinite loop that continues to ask for a guess from user with a try andn except to ensure integers are entered

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from random import randint
 lower_num, higher_num = 1, 10
 random_number: int = randint(lower_num, higher_num)
 print (f'Guess the number in the range from {lower_num} to {higher_num}.')
+
+while True:
+    try:
+        user_guess: int = ing(input('Guees: '))
+    except ValueError as e:
+        print('Please enter a valid number.')
+        continue

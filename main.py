a703837114c4d3d12d30535f604f53e04d50d6fc
VilMe <vilma.medrano00@gmail.com>
2025-09-04 23:15:19 -0500

    changed conditional and updated text output

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randint
 lower_num, higher_num = 1, 10
 random_number: int = randint(lower_num, higher_num)
 number_of_attempts = 1
-print (f'Guess the number in the range from {lower_num} to {higher_num}.')
+print (f'Guess the number in the range from {lower_num} to {higher_num}. You have only 3 tries')
 
 while True:
     try:
@@ -12,15 +12,15 @@ while True:
         print('Please enter a valid number.')
         continue
 
-    if user_guess > random_number and number_of_attempts <=3:
-        print('The number is lower')
+    if user_guess > random_number and number_of_attempts < 3:
+        print(f'The number is lower')
         number_of_attempts +=1
-    elif user_guess < random_number and number_of_attempts <=3:
-        print ('The number is higher')
+    elif user_guess < random_number and number_of_attempts < 3:
+        print (f'The number is higher ')
         number_of_attempts +=1
-    elif number_of_attempts == 3:
-        print("You had you're 3 tries, game over")
+    elif user_guess != random_number and number_of_attempts == 3:
+        print(f"You had you're 3 tries, game over!")
         break
     else:
-        print('You guessed it!')
+        print(f'You guessed it!')
         break
